a816026b826e245eefb0a78475fadd67998f2e64
Angharad Caswell <ange.caswell1@gmail.com>
2022-06-12 15:17:09 +0000

    Added session data to dashboard

--- volunteer_dashboard/urls.py
+++ volunteer_dashboard/urls.py
@@ -3,6 +3,5 @@ from .views import VolunteerList
 
 urlpatterns = [
     path('', VolunteerList.as_view(), name='volunteer_list'),
-    path('', SessionList.as_view(), name='session_list'),
 
 ]
--- volunteer_dashboard/views.py
+++ volunteer_dashboard/views.py
@@ -5,11 +5,24 @@ from volunteer_app.models import VolunteerProfile, Session
 
 class VolunteerList(generic.ListView):
     template_name = 'dashboard.html'
-    queryset = VolunteerProfile.objects.all()
+    context_object_name = 'volunteer_list'
+    model = VolunteerProfile
 
-class SessionList(generic.ListView):
-    template_name = 'dashboard.html'
-    queryset = Session().objects.all()
+    def get_queryset(self):
+        return VolunteerProfile.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(VolunteerList, self).get_context_data(**kwargs)
+        context['session_list'] = Session.objects.all()
+        return context
+
+    
+
+# class SessionList(generic.ListView):
+#     template_name = 'dashboard.html'
+#     model = Session
+#     queryset = Session.objects.all()
+#     context_object_name = 'session_list'
 
 
 
